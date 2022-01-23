696a0a0d1ba5a509ea11d9284051ff5460618880
deokgoo <kkddgg1001@gmail.com>
2022-01-24 00:58:20 +0900

    fix: og-image 수정

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/deokgoo/next-duck-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/main-logo-no-bg.png',
   email: 'kkddgg1001@gmail.com',
   github: 'https://github.com/deokgoo',
   twitter: 'https://twitter.com/Twitter',
