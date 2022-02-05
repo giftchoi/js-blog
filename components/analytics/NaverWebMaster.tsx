3672ac0d309354afe14d0594ac6d0ed6bcb21dfe
deokgoo <kkddgg1001@gmail.com>
2022-02-05 17:44:33 +0900

    fix: 네이버웹 마스터 tag 수정

--- components/analytics/NaverWebMaster.tsx
+++ components/analytics/NaverWebMaster.tsx
@@ -1,14 +1,11 @@
-import Script from 'next/script'
-
+import Head from 'next/head'
 import siteMetadata from '@/data/siteMetadata'
-
-<meta name="naver-site-verification" content="3884d7b87ded12a75e3e2ba6a101a34bebcf4715" />
 
 const NaverWebMaster = () => {
   return (
-    <>
-      <Script strategy="lazyOnload" src={siteMetadata.analytics.naverWebMasterId} />
-    </>
+    <Head>
+      <meta name="naver-site-verification" content={siteMetadata.analytics.naverWebMasterId} />
+    </Head>
   )
 }
 
