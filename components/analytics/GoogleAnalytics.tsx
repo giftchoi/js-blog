fb32350cbcfedfce342227802abcada4d301b439
deokgoo <kkddgg1001@gmail.com>
2022-02-19 23:47:00 +0900

    feature: google search console verification

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -1,10 +1,17 @@
 import Script from 'next/script'
-
+import Head from 'next/head'
 import siteMetadata from '@/data/siteMetadata'
 
 const GAScript = () => {
   return (
     <>
+      <Head>
+        {/* google search console 인증을 위함 */}
+        <meta
+          name="google-site-verification"
+          content="b_LbgCHHUhfkQ-P2Ug9l9U-e6Pyp6n8hzmR9aubp8Ec"
+        />
+      </Head>
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
