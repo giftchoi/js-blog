7d84625111951723c0f67bc571e75c518f909d4d
deokgoo <kkddgg1001@gmail.com>
2022-03-27 23:54:37 +0900

    post: programmers challenge

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,11 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ ...rest }: ImageProps) => (
+  <NextImage
+    {...rest}
+    placeholder="blur"
+    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+  />
+)
 
 export default Image
