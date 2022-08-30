50412f358f32e685d93ed487abf02557036c9a5f
deokgoo <kkddgg1001@gmail.com>
2022-08-30 23:29:13 +0900

    feature: wip/diet-battle

---
+++ components/ApexChart.tsx
@@ -0,0 +1,45 @@
+import ApexCharts from 'react-apexcharts'
+import { useState } from 'react'
+import { ApexOptions } from 'apexcharts'
+
+const DietChart = () => {
+  const [series, setSeries] = useState<ApexAxisChartSeries>([
+    {
+      name: '덕구',
+      data: [10, 20, 30, 50, 100],
+    },
+    {
+      name: '지선',
+      data: [10, 30, 50, 80, 100],
+    },
+  ])
+  const options: ApexOptions = {
+    chart: {
+      zoom: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'straight',
+    },
+    title: {
+      text: 'Product Trends by Month',
+      align: 'left',
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: ['1', '2', '3', '4', '5', '6', '7'],
+    },
+  }
+  return <ApexCharts series={series} options={options} />
+}
+
+export default DietChart
---
+++ pages/diet-battle.tsx
@@ -0,0 +1,28 @@
+import ThinkIcon from '@/components/icos/think'
+import { useEffect } from 'react'
+import ApexCharts from '@/components/ApexChart'
+
+const DietBattle = () => {
+  useEffect(() => {
+    console.log('asdf')
+  }, [])
+
+  return (
+    <div className="mt-3 text-4xl">
+      <div className="grid grid-cols-6 gap-4">
+        <div className="col-start-1 col-end-3 text-center font-bold">
+          <span className="text-amber-500">Diet </span>battle
+        </div>
+        <div className="col-end-7 col-span-2 flex justify-center text-yellow-200">
+          <ThinkIcon />
+        </div>
+        <div className="col-start-2 col-span-4 text-center">Instructions</div>
+        <div className="col-start-2 col-span-4 text-center">
+          <ApexCharts />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DietBattle
