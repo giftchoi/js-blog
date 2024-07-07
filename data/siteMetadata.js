06dc4d924ff6babdd8dfb0e39874526f2c98edf0
giftchoi <giftchoi02@naver.com>
2024-07-07 22:37:03 +0900

    google id change

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -41,7 +41,7 @@ const siteMetadata = {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: 'G-34CEVX8HBY',
+      googleAnalyticsId: 'G-X53EPGY5NN',
     },
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
